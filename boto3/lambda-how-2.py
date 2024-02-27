b0b29cdf86dfd7dc1a82e8b0972b26b426717907
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2024-02-27 06:57:31 +0530

    Update lambda-how-2.py

--- boto3/lambda-how-2.py
+++ boto3/lambda-how-2.py
@@ -16,6 +16,12 @@
 #     # DO somwting
 #     return some_value
 
+# # The first argument is the event object. An event is a 
+# # JSON-formatted document that contains data for a Lambda function to process.
+# #  The Lambda runtime converts the event to an object and passes it to your 
+# # function code. It is usually of the Python dict type. It can also be list, str, int, float, or the NoneType type.
+# # aws service ytype defines event type
+
 event = {
     "year": 2000
 }
@@ -29,3 +35,5 @@ def lambda_handler(event,context):
     print("***********************************************")
 
 lambda_handler(event,context=[])
+
+# returning value = https://docs.aws.amazon.com/lambda/latest/dg/python-handler.html#python-handler-return
