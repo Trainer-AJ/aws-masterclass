ba3d4dec5276ced90744c2930f27322b862c8569
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2024-09-04 18:48:45 +0530

    Add files via upload

---
+++ boto3/env-lambda/test.py
@@ -0,0 +1,21 @@
+# pip install python-dotenv
+
+import os 
+import boto3 
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+def test_env():
+    print(f"All Env detected => {os.getenv("region")}, {os.getenv("min")}")
+
+def s3(): 
+    region = os.getenv("region")
+    s3 = boto3.client('s3', region_name=region)
+
+    response = s3.list_buckets()
+    print(response)
+
+def lambda_handler(event, context):
+test_env()
+s3()
