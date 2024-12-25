b5928132ad99a6b554c73af062190908c8222cb3
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2024-12-26 00:32:40 +0530

    added multi region support

--- utility/s3-empty-delete.py
+++ utility/s3-empty-delete.py
@@ -1,16 +1,10 @@
 import boto3
 
-# Set the region
-region = 'ap-south-1'
-
-# Initialize the S3 client
-s3_client = boto3.client('s3', region_name=region)
-
-def list_buckets():
+def list_buckets(s3_client):
     response = s3_client.list_buckets()
     return [bucket['Name'] for bucket in response['Buckets']]
 
-def empty_bucket(bucket_name):
+def empty_bucket(s3_client,bucket_name):
     # List objects in the bucket
     response = s3_client.list_objects_v2(Bucket=bucket_name)
     
@@ -28,19 +22,34 @@ def empty_bucket(bucket_name):
         objects = [{'Key': obj['Key']} for obj in response['Contents']]
         s3_client.delete_objects(Bucket=bucket_name, Delete={'Objects': objects})
 
-def delete_bucket(bucket_name):
-    empty_bucket(bucket_name)
+def delete_bucket(s3_client,bucket_name):
+    empty_bucket(s3_client,bucket_name)
     s3_client.delete_bucket(Bucket=bucket_name)
 
+def logic(s3_client,buckets):
+# buckets is an empty list so ...
+        if buckets != []:
+            # Filter buckets based on the region if necessary
+            for bucket in buckets:
+                print(f"Processing bucket: {bucket} \n")
+                delete_bucket(s3_client,bucket)
+                print(f"Deleted bucket: {bucket} \n")
+        else:
+            print(f"No bucket found !!")
+
 def main():
-    # List all buckets
-    buckets = list_buckets()
-    
-    # Filter buckets based on the region if necessary
-    for bucket in buckets:
-        print(f"Processing bucket: {bucket}")
-        delete_bucket(bucket)
-        print(f"Deleted bucket: {bucket}")
+    # Set the region
+    # region = 'ap-south-1'
+    region = ['ap-south-1','us-west-1']
+
+    for var in region:
+    # Initialize the S3 client
+        s3_client = boto3.client('s3', region_name=var)
+        # List all buckets and save them in here
+        buckets = list_buckets(s3_client)
+        
+        logic(s3_client,buckets)
+
 
 if __name__ == "__main__":
     main()
