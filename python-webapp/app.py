aafe31f04021cb1fcac8467c120b25278a97a9ad
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2025-01-09 20:45:20 +0530

    Update app.py

--- python-webapp/app.py
+++ python-webapp/app.py
@@ -39,4 +39,4 @@ def employee():
         return render_template('index.html', message="Employee not found!")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port='80',debug=True)
